4018c4a0c203d37b8b0505136715c1c506248dc8
Alberto Valverde Gonzalez <alberto@meteogrid.com>
2014-10-16 15:03:17 +0200

    fixed

--- build.sh
+++ build.sh
@@ -6,7 +6,5 @@ git submodule init
 git submodule update
 $CABAL sandbox init
 $CABAL sandbox add-source sigym4-geometry
-$CABAL sandbox add-source persistent/persistent
-$CABAL sandbox add-source persistent/persistent-postgresql
 $CABAL install --only-dependencies  $@
 $CABAL build
